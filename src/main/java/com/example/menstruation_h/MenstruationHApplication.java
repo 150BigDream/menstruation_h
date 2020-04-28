1290cfa1b3065c528a467cda9bdc87f98536beff
150BigDream <2316624245@qq.com>
2020-04-28 17:37:17 +0800

    first commit

---
+++ src/main/java/com/example/menstruation_h/MenstruationHApplication.java
@@ -0,0 +1,13 @@
+package com.example.menstruation_h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenstruationHApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenstruationHApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/menstruation_h/MenstruationHApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.menstruation_h;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenstruationHApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
